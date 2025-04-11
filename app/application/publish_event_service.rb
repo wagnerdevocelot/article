14edb1e562cd81cc2184cc5ee5da5856027aecda
vapordev <wagner.dev1337@gmail.com>
2025-04-11 00:19:29 -0300

    example

--- app/application/publish_event_service.rb
+++ app/application/publish_event_service.rb
@@ -1,8 +1,8 @@
 # application/publish_event_service.rb
 require_relative '../domain/event'
-require_relative '../infrastructure/kafka_event_publisher'
+require_relative '../infrastructure/kafka_event_publisher' # viola o princípio de isolamento de camadas trazendo a infraestrutura para a camada de aplicação
 class PublishEventService
-  def initialize(publisher: KafkaEventPublisher.new)
+  def initialize(publisher: KafkaEventPublisher.new) # viola o princípio de isolamento de camadas trazendo a infraestrutura para a camada de aplicação
     @publisher = publisher
   end
 
